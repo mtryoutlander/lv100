2e610562daaa5b0c8f8c638ed80f3aff59adb699
mtryoutlander <32582120+mtryoutlander@users.noreply.github.com>
2023-04-19 19:01:53 -0500

    fix names of some the script and add animations for brute and logic

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,11 +7,11 @@ public class PlayerControler : MonoBehaviour
 {
     //get input from new input system
     private PlayerInput input;
-    private Movement movement;
+    private PlayerMovemetnControler movement;
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
-        movement = GetComponent<Movement>();
+        movement = GetComponent<PlayerMovemetnControler>();
     }
 
     private void Update()
